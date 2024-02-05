72889c466791c09f150f1339101d9331e1408f0d
rohinibhosalernb <113599488+rohinibhosalernb@users.noreply.github.com>
2024-02-05 16:54:30 +0530

    done

---
+++ src/Array_02_02_assign/Second_largest_no.java
@@ -0,0 +1,37 @@
+package Array_02_02_assign;
+
+public class Second_largest_no {
+
+	public static void main(String[] args) {
+		int arr[]= new int[] {10,25,77,8,66,45,33,21};
+		System.out.println("Before sorting array elements" );
+		for(int i=0; i< arr.length ; i++)
+		{
+			System.out.print(arr[i] +",");
+		}
+		int temp =0;
+		for(int i=0; i<arr.length;i++)
+		{
+		for(int j=i+1;j<arr.length;j++)
+		{
+			if(arr[i]>arr[j])
+			{
+				temp= arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+			
+		}
+		}
+		System.out.println();
+		System.out.println("After sorting array elements" );
+		for(int i=0; i< arr.length ; i++)
+		{
+			System.out.print(arr[i] +",");
+		}
+		System.out.println();
+		System.out.println("Second largest element in array ="+" "+ arr[1]);
+		 
+	}
+
+}
